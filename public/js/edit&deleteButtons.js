f63ca988b163a4257515dd3fca8c0ec08403f5aa
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-06-15 20:44:56 +0300

    fix some bugs in adminProfile

--- public/js/edit&deleteButtons.js
+++ public/js/edit&deleteButtons.js
@@ -1,22 +1,22 @@
 //select overlay pages 
 let editOverlay = document.querySelector('.edit-overlay');
-// let deleteOverlay = document.querySelector('.delete-overlay')
+let deleteOverlay = document.querySelector('.delete-overlay')
 
 //select cancel buttons in edit and delete overlay
-// var cancelDeletion = document.querySelector('.cancel-deletion')
-// var cancelEdit = document.querySelector('.cancel-edit')
+var cancelDeletion = document.querySelector('.cancel-deletion')
+var cancelEdit = document.querySelector('.cancel-edit')
 
 //make overlays dissapears when cancel buttons are been clicked
-// cancelDeletion.addEventListener('click',function(){
-//     deleteOverlay.style.display = 'none'
-// })
-// cancelEdit.addEventListener('click',function(){
-//     editOverlay.style.display = 'none'
-// })
+cancelDeletion.addEventListener('click',function(){
+    deleteOverlay.style.display = 'none'
+})
+cancelEdit.addEventListener('click',function(){
+    editOverlay.style.display = 'none'
+})
 
 // select all delete and edit buttons in all 3Dots menus
 let editButtons  = document.querySelectorAll('.edit-btn')
-// let deleteButtons = document.querySelectorAll('.delete-btn')
+let deleteButtons = document.querySelectorAll('.delete-btn')
 
 // make them open edit or delete overlay pages when be clicked
 editButtons.forEach((editButton)=>{
@@ -24,8 +24,8 @@ editButtons.forEach((editButton)=>{
         editOverlay.style.display = 'flex'
     })
 })
-// deleteButtons.forEach((deleteButton)=>{
-//     deleteButton.addEventListener('click',function(){
-//         deleteOverlay.style.display = 'flex'
-//     })
-// })
+deleteButtons.forEach((deleteButton)=>{
+    deleteButton.addEventListener('click',function(){
+        deleteOverlay.style.display = 'flex'
+    })
+})
