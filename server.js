12250bbf8747280f858eeb697751f2bc40f0c3cc
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-06-21 19:05:49 +0300

    add delete functionality to the patientProfile page

--- server.js
+++ server.js
@@ -1731,6 +1731,26 @@ app.post("/adminProfileDelete",async (req,res)=>{
             })
     })
 })
+
+app.post("/patientProfilePage",async (req,res)=>{
+    let oldid = req.body.oldid;
+    await pool.query("delete from patient where nationalid = $1",[oldid], async(err, rp) => {
+        let dataNumbers = await getNumbers()
+        res.render("./homePage.ejs",
+            {
+                name: req.session.user["username"],
+                image: req.session.user["image"],
+                dataNumbers: dataNumbers,
+                show:  null, error: "",
+                show1:  null, addSurgeonError: "",
+                show2:  null, addPatientError: "",
+                show3:  null, addAdminError: "",
+                show4:  null, addOperationError: "",
+                show5:  null, addDeviceError: "",
+                show6:  null, addAppointmentError: ""
+            })
+    })
+})
  
 app.listen(port, (req, res) => {
   console.log(`server is running on port number ${port}`);
